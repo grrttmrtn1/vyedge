1be859ba06ec58cc1809a5987a6673c71b503248
Garrett Martin <grrttmrtn2@gmail.com>
2026-06-16 22:37:53 -0500

    chore: remove 13 dead code files (unused pages, Layout, debug scripts)

--- list_users.ts
+++
@@ -1,5 +0,0 @@
-
-import Database from "better-sqlite3";
-const db = new Database("vy_control.db");
-const users = db.prepare("SELECT id, username, role FROM users").all();
-console.log("Users in DB:", JSON.stringify(users, null, 2));
